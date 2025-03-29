28ae7fe8bb456e0df8c21c231a15a352501a1efc
BGV <26331505+bgv2@users.noreply.github.com>
2025-03-29 19:29:41 -0400

    call page with duration

---
+++ React/src/app/call/page.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import React from 'react';
+
+const CallPage = () => {
+  const [callDuration, setCallDuration] = React.useState(0);
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setCallDuration((prev) => prev + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <h1 className="text-4xl font-bold text-center sm:text-left">
+        Call with a Definitely Real Person</h1>
+
+        <div className="text-2xl font-bold">{callDuration}s</div>
+        <button className="bg-red-500 text-white rounded-md p-2">Emergency</button>
+        <button className="bg-blue-500 text-white rounded-md p-2"
+        onClick={() => {
+          window.location.href = '/';
+        }}>
+        End
+        </button>
+      </main>
+    </div>
+  );
+};
+
+export default CallPage;
